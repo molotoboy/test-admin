fb3e7eced19cad8685d897794af7beeb55ce8fa9
molotoboy <molotoboy@gmail.com>
2020-04-18 00:09:39 +0600

    add PostFilter

--- src/posts.js
+++ src/posts.js
@@ -11,10 +11,20 @@ import {
   SelectInput,
   TextInput,
   Create,
+  Filter,
 } from "react-admin";
 
+const PostFilter = (props) => (
+  <Filter {...props}>
+    <TextInput label="Search" source="q" alwaysOn />
+    <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+      <SelectInput optionText="name" />
+    </ReferenceInput>
+  </Filter>
+);
+
 export const PostList = (props) => (
-  <List {...props}>
+  <List filters={<PostFilter />} {...props}>
     <Datagrid>
       <TextField source="id" />
       <ReferenceField source="userId" reference="users">
