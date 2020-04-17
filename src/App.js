38061b890a01bee80032803004f799b199ff1d0f
molotoboy <molotoboy@gmail.com>
2020-04-18 00:13:52 +0600

    add Icons in Menu

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Admin, Resource } from "react-admin";
+import PostIcon from "@material-ui/icons/Book";
+import UserIcon from "@material-ui/icons/Group";
 import jsonServerProvider from "ra-data-json-server";
 
 import { UserList } from "./users";
@@ -13,8 +15,9 @@ const App = () => (
       list={PostList}
       edit={PostEdit}
       create={PostCreate}
+      icon={PostIcon}
     />
-    <Resource name="users" list={UserList} />
+    <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
 );
 
