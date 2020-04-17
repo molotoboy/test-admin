4cecebaebefe3b88cafb02e673de23ce7905e245
molotoboy <molotoboy@gmail.com>
2020-04-18 00:58:33 +0600

    add authProvider

--- src/App.js
+++ src/App.js
@@ -7,10 +7,15 @@ import jsonServerProvider from "ra-data-json-server";
 import { UserList } from "./users";
 import { PostList, PostEdit, PostCreate } from "./posts";
 import Dashboard from "./Dashboard";
+import authProvider from "./authProvider";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 const App = () => (
-  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+  <Admin
+    dashboard={Dashboard}
+    authProvider={authProvider}
+    dataProvider={dataProvider}
+  >
     <Resource
       name="posts"
       list={PostList}
